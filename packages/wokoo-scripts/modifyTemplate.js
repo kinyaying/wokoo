ea99c9bb1decfab386be0e559f77aa888227d92a
sven <hellosc@qq.com>
2021-03-16 15:22:05 +0800

    fix: 删除测试代码

--- packages/wokoo-scripts/modifyTemplate.js
+++ packages/wokoo-scripts/modifyTemplate.js
@@ -16,7 +16,6 @@ render = promisify(render) // 包装渲染方法
     }
  */
 async function handleTemplate(fromPath, toPath, config) {
-  fromPath = '/Users/kin/MyCode/wokoo/packages/wokoo-template/react-template'
   console.log('fromPath::', fromPath)
 
   await new Promise((resovle, reject) => {
